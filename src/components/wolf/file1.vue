af271ee6a89c31ceae1365ffe8c869a836de10cb
shota-kamezawa <75684759+shota-kamezawa@users.noreply.github.com>
2022-01-27 10:33:12 +0000

    🔄 sync with shota-kamezawa/base_repository/dca3a7d6f0d12bdfe73b0cc476ada817b76290f6

---
+++ src/components/wolf/file1.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img
+      :src="require('@/assets/icons/dummy.svg')"
+      alt="dummy"
+    >
+    <img
+      :src="require('@/assets/icons/dummy.svg')"
+      alt="dummy"
+    >
+  </div>
+</template>
+
+<script>
+import Dummy from '@/components/atoms/dummy';
+
+export default {
+  components: {
+    Dummy,
+  },
+};
+</script>
